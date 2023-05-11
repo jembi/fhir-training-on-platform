7c66c3e9bfe3d9f6bfd2569bfa9b47e3b1ce8113
michaelloosen <thorne199@gmail.com>
2023-05-11 15:06:18 +0200

    Added an easy way to specify the remove action and operating system For other OS users to be able to use these scripts and for ease of specifying deploy action.

--- replace-ig-local.sh
+++ replace-ig-local.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
-./instant-linux package remove --profile=local-remove-fhir
+operating_system=${1:-linux}
+other_params=${*:2}
 
-./instant-linux package init --profile=local-deploy-ig
+if [ -n "$other_params" ]; then
+    echo "Incorrect parameter(s): $other_params. Usage: replace-ig-local.sh [linux|macos|windows]"
+    exit 0
+fi
+
+case ${operating_system} in
+linux)
+  echo "Running with the Linux CLI binary"
+  ./instant-linux package remove --profile=local-remove-fhir
+  ./instant-linux package init --profile=local-deploy-ig
+  exit 0
+  ;;
+macos)
+  echo "Running with the MacOS CLI binary"
+  ./instant-macos package remove --profile=local-remove-fhir
+  ./instant-macos package init --profile=local-deploy-ig
+  exit 0
+  ;;
+windows)
+  echo "Running with the Windows CLI binary"
+  ./instant.exe package remove --profile=local-remove-fhir
+  ./instant.exe package init --profile=local-deploy-ig
+  exit 0
+  ;;
+--help)
+  echo "Usage: replace-ig-local.sh [linux|macos|windows]"
+  exit 0
+  ;;
+*)
+  echo "Usage: replace-ig-local.sh [linux|macos|windows]"
+  exit 0
+  ;;
+esac
